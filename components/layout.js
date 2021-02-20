ffd3d87c3af76765612664d1fd1f2b191a8d45e8
katherinepeterson <katherine.peterson@yahoo.com>
2021-02-19 20:36:15 -0800

    Create layout and nav

--- components/layout.js
+++ components/layout.js
@@ -2,19 +2,27 @@ import Link from 'next/link'
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col items-center justify-center h-screen text-gray-800 bg-gradient-to-b from-lightBlue-200 to-white">
+      <div className="flex justify-center my-6 leading-6">
         <Link href="/">
-          <a>Home</a>
+          <button className="py-1 text-xs leading-6 tracking-widest border border-gray-400 rounded-full w-28">
+            HOME
+          </button>
         </Link>
         <Link href="/about">
-          <a>About</a>
+          <button className="py-1 mx-2 text-xs leading-6 tracking-widest border border-gray-400 rounded-full w-28">
+            ABOUT
+          </button>
         </Link>
         <Link href="/projects">
-          <a>Projects</a>
+          <button className="py-1 text-xs leading-6 tracking-widest border border-gray-400 rounded-full w-28">
+            PROJECTS
+          </button>
         </Link>
       </div>
-      {children}
+      <div className="flex flex-col items-center justify-center w-full max-w-3xl border-t border-b border-gray-400">
+        <div className="flex justify-center">{children}</div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      lightBlue: colors.lightBlue,
+      blue: colors.blue,
+      gray: colors.gray,
+    },
   },
   variants: {
     extend: {},
