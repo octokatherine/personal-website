735d4d9d28a4ab91e7cbe5eff3bc13b336c0642d
Katherine Oelsner <octokatherine@github.com>
2022-09-04 12:30:22 -0700

    remove blog links

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,13 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import { faGithub, faTwitter, faLinkedinIn, faDev } from '@fortawesome/free-brands-svg-icons'
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { AnimatePresence, motion } from 'framer-motion'
+import { faDev, faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { motion, AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 import { useEffect } from 'react'
 import { useLocalStorage } from '../hooks/useLocalStorage'
-import dynamic from 'next/dynamic'
 const DarkModeToggle = dynamic(() => import('dark-mode-toggle-animation'), { ssr: false })
 
 export default function Layout({ children }) {
@@ -101,20 +102,6 @@ export default function Layout({ children }) {
             target="_blank"
           >
             <FontAwesomeIcon className="mr-6 text-2xl" icon={faLinkedinIn} />
-          </a>
-          {/* <a
-            className="text-gray-400 dark:text-white hover:text-lightBlue-600 dark:hover:text-pink-500"
-            href="https://dev.to/katherinecodes"
-            target="_blank"
-          >
-            <FontAwesomeIcon className="mr-6 text-2xl" icon={faDev} />
-          </a> */}
-          <a
-            className="text-gray-400 dark:text-white hover:text-lightBlue-600 dark:hover:text-pink-500"
-            href="https://blog.katherinempeterson.com"
-            target="_blank"
-          >
-            <FontAwesomeIcon className="mr-6 text-2xl" icon={faPencilAlt} />
           </a>
         </motion.div>
       </div>
